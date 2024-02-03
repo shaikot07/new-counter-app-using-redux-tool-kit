b1265b7cad64383a7695ec5339cf756278a1bef3
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-03 21:44:16 +0600

    setup new counter app and i use here redux tool kit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="p-6">
+      <h1 className='text-4xl text-center text-[#8e44ad] font-semibold'>New  Counter app using redux tool-kit </h1>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
