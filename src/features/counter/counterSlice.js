bae7ee7217434bd64c1ccc4c734947052574c74f
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-04 23:55:06 +0600

    added counter clices

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+
+const counterSlice = createSlice({
+      name: "counter",
+      initialState:{count:0},
+      reducers:{
+            increment: (state )=>{
+                  state.count = state.count +1
+            },
+            decrement:(state)=>{
+                  state.count = state.count - 1
+            },
+            reset:(state)=>{
+                  state.count=0
+            },
+      }
+})
